53700bf0c63e7a4145c9c6bdc6cb5dc633fc8014
Heitor-Grande <heitorogrande2001@gmail.com>
2025-01-15 19:16:06 -0300

    comentando

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,11 +4,10 @@ import { userService } from "./user.service";
 //Controller => rota de acesso "/user"
 @Controller("user")
 export class userController {
-
+    //cria um constructor do tipo userService para acessar os metodos do meu service
     constructor(private readonly userService: userService) { }
-
     @Post()
-    //função para criar novo user
+    //metodo que vai receber o user e chamar o metodo do service para aplicar regra de negocio
     createUser(@Body() user: userDto) {
         this.userService.create(user)
     }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { userDto } from "./user.dto";
 @Injectable()
 export class userService {
     private users: userDto[] = []
+    //crio a função do service para realizar a regra de negocio de fato
     create(user: userDto) {
         this.users.push(user)
         console.log(this.users)
